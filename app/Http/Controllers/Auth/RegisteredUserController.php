fa6af80bab514cb9bba8deaa9501c084e069a422
shaikh-mohd-samir <mohdsameershaikh2003@gmail.com>
2026-05-08 21:46:12 +0530

    change register redirect

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,8 +44,7 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
-        return redirect(route('dashboard', absolute: false));
+        return redirect('/login')
+            ->with('status', 'Registration successful. Please login.');
     }
 }
